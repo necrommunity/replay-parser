69b1dd501a9e2e5d3e8659b51d3abe79ae2a3d02
Alexis Martin <alexis.yj@gmail.com>
2017-07-22 20:32:46 +0200

    Update parsing for new replay file version on the DLC full release

--- js/main.js
+++ js/main.js
@@ -56,9 +56,12 @@ var getZoneForChar = function(songs, char1, type, infos) {
 		case '8': //bolt
 		case '9': //bard
 		case '10': //nocturna
-			var zone = numType < 5 ? numType : Math.floor((songs-1)/4) + 1,
+		case '11': //diamond
+		case '12': //mary
+		case '13': //tempo
+			var zone = (numType < 5 && numType > 0) ? numType : Math.floor((songs-1)/4) + 1,
 				floor = ((songs-1) % 4) + 1;
-			if(char1 === '2' && numType >= nbZones+1) zone = (nbZones+1) - zone;
+			if(char1 === '2' && (numType >= nbZones+1 || numType < 5)) zone = (nbZones+1) - zone;
 			if(zone > nbZones) {
 				if(zone > nbZones+1 || floor > 2) {
 					infos.bugged = 'NB_SONGS';
@@ -74,7 +77,7 @@ var getZoneForChar = function(songs, char1, type, infos) {
 			}
 			return zone + '-' + floor;
 		case '6': //dove
-			return (numType < nbZones+1 ? numType : (Math.floor((songs-1)/3) + 1)) + '-' +(((songs-1) % 3) + 1);
+			return (numType < nbZones+1 && numType > 0 ? numType : (Math.floor((songs-1)/3) + 1)) + '-' +(((songs-1) % 3) + 1);
 	}
 };
 
@@ -91,19 +94,39 @@ var getCharName = function(n) {
 		case '8': return 'bolt';
 		case '9': return 'bard';
 		case '10': return 'nocturna';
+		case '11': return 'diamond';
+		case '12': return 'mary';
+		case '13': return 'tempo';
 	}
 }
-var getTypeName = function(n, amplified, splitedData) {
-
+var getTypeName = function(n, amplified, splitedData, infos) {
 	switch(n) {
-		case '1': return 'Zone 1';
+		case '1': 
+			if(infos && infos.name10) {
+				var n = infos.name10;
+				delete infos.name10;
+				return getTypeName(n, true, splitedData, infos);
+			}
+			return 'Zone 1';
 		case '2': return 'Zone 2';
 		case '3': return 'Zone 3';
 		case '4': return 'Zone 4';
-		case '5': return (amplified && parseInt(splitedData[1], 10) == 5) ? 'Zone 5' : 'Dance Pad';
-		case '6': return 'All-Zones';
-		case '7': return 'Daily';
-		case '8': return 'Seeded All-Zones';
+		case '5': return ((amplified && parseInt(splitedData[1], 10) == 5) || (infos.amplifiedFullRelease && parseInt(splitedData[2], 10) == 5)) ? 'Zone 5' : 'Dance Pad';
+		case '-7': case '6': return 'All-Zones';
+		case '-8': return 'Dance Pad';
+		case '-9': case '7': return 'Daily';
+		case '-10': case '8': return 'Seeded All-Zones';
+		case '-50': return 'Story Mode';
+		case '-52': return 'No Return';
+		case '-53': return 'Seeded No Return';
+		case '-55': return 'Hard Mode';
+		case '-56': return 'Seeded Hard Mode';
+		case '-59': return 'Phasing';
+		case '-60': return 'Randomizer';
+		case '-61': return 'Mystery';
+		case '-62': return 'Seeded Phasing';
+		case '-63': return 'Seeded Randomizer';
+		case '-64': return 'Seeded Mystery';
 		default: return 'Unknown';
 	}
 }
@@ -112,7 +135,7 @@ var getTypeName = function(n, amplified, splitedData) {
 var showData = function(data, splitedData) {
 	var html = '<ul>';
 
-	if(data.seed && (!data.type || parseInt(data.type, 10) > 5)) {
+	if(data.seed && (!data.type || parseInt(data.type, 10) > 5 || parseInt(data.type, 10) < 1)) {
 		html += '<li><label>Seed:</label> '+data.seed+'</li>';
 	}
 	if(data.char1) {
@@ -122,7 +145,7 @@ var showData = function(data, splitedData) {
 		html += '<li><label>Second Character:</label> '+getCharName(data.char2)+'</li>';
 	}
 	if(data.type) {
-		html += '<li><label>Type:</label> '+getTypeName(data.type, data.amplified, splitedData)+'</li>';
+		html += '<li><label>Type:</label> '+getTypeName(data.type, data.amplified, splitedData, data)+'</li>';
 	}
 	if(data.formatedTime) {
 		html += '<li><label>Time:</label> '+data.formatedTime+'</li>';
@@ -137,6 +160,13 @@ var showData = function(data, splitedData) {
 	document.getElementById('output').innerHTML = html + '</ul>';
 };
 
+var getSplitIndex = function(infos, index) {
+	if(infos.amplifiedFullRelease) {
+		return index - 3;
+	}
+	return index;
+}
+
 var parseReplayFile = function(file) {
 	var infos = {},
 		filename = file.name
@@ -144,6 +174,7 @@ var parseReplayFile = function(file) {
 		splitName = name.split('_');
 	infos.version = splitName[0];
 	infos.amplified = parseInt(infos.version, 10) > 75;
+	infos.amplifiedFullRelease = parseInt(infos.version, 10) > 84;
 	infos.filename = file.name;
 	infos.date = new Date(
 		parseInt(splitName[3], 10),
@@ -155,23 +186,27 @@ var parseReplayFile = function(file) {
 	);
 	infos.type = splitName[9];
 
+	if(infos.amplifiedFullRelease) {
+		infos.name10 = splitName[10];
+	}
+
 	var reader = new FileReader();
 	reader.onload = function(theFile) {
 		data = theFile.currentTarget.result
 		var splitedData = data.split('\\n');
-		if(splitedData[8]) {
-			infos.time = parseInt(splitedData[8], 10);
+		if(splitedData[getSplitIndex(infos, 8)]) {
+			infos.time = parseInt(splitedData[getSplitIndex(infos, 8)], 10);
 			infos.formatedTime = formatTime(infos.time);
 		}
-		if(splitedData[10]) infos.seed = calculateSeed(splitedData[10], infos.amplified);
-		if(splitedData[11]) infos.players = parseInt(splitedData[11], 10);
-		if(splitedData[17]) infos.char1 = splitedData[15].split('|')[0];
-		if(infos.players > 1 && splitedData[17]) {
-			infos.char2 = splitedData[17].split('|')[0];
+		if(splitedData[getSplitIndex(infos, 10)]) infos.seed = calculateSeed(splitedData[getSplitIndex(infos, 10)], infos.amplified);
+		if(splitedData[getSplitIndex(infos, 11)]) infos.players = parseInt(splitedData[getSplitIndex(infos, 11)], 10);
+		if(splitedData[getSplitIndex(infos, 17)]) infos.char1 = splitedData[getSplitIndex(infos, 15)].split('|')[0];
+		if(infos.players > 1 && splitedData[getSplitIndex(infos, 17)]) {
+			infos.char2 = splitedData[getSplitIndex(infos, 17)].split('|')[0];
 		}
-		if(splitedData[9]) {
-			infos.songs = parseInt(splitedData[9], 10);
-			infos.endZone = getZoneForChar(parseInt(splitedData[9], 10), infos.char1, infos.type, infos);
+		if(splitedData[getSplitIndex(infos, 9)]) {
+			infos.songs = parseInt(splitedData[getSplitIndex(infos, 9)], 10);
+			infos.endZone = getZoneForChar(parseInt(splitedData[getSplitIndex(infos, 9)], 10), infos.char1, infos.type, infos);
 		}
 		console.log(infos);
 		showData(infos, splitedData);
